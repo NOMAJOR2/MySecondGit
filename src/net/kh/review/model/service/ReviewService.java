2f5824c49f593b68fe38d38ade3acaf8434c64a6
iccon <iccon@DESKTOP-5759EEA>
2021-09-17 11:30:27 +0900

    reviewservice dao object 추가

--- src/net/kh/review/model/service/ReviewService.java
+++ src/net/kh/review/model/service/ReviewService.java
@@ -1,5 +1,12 @@
 package net.kh.review.model.service;
 
+import net.kh.review.model.dao.ReviewDAO;
+
 public class ReviewService {
 
+private ReviewDAO rDao;
+	
+	public ReviewService() {
+		rDao = new ReviewDAO();
+	}
 }
